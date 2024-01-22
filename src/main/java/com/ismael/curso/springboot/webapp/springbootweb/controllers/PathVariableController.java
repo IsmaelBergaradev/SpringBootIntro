cd69e40b486d9c610cef0deb0429224aac3e778d
IsmaelBergaradev <isbergara1@gmail.com>
2024-01-22 17:59:02 -0300

    PATH

--- src/main/java/com/ismael/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/ismael/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -2,23 +2,73 @@ package com.ismael.curso.springboot.webapp.springbootweb.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ismael.curso.springboot.webapp.springbootweb.models.user;
 import com.ismael.curso.springboot.webapp.springbootweb.models.dto.ParamDto;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+
+
 
 
 
 @RestController
 @RequestMapping("/api/var")
-
 public class PathVariableController {
+    
+    @Value("${config.username}")
+    private String username;
+
+    @Value("${config.message}")
+    private String message;
+
+    @Value("${config.listOfValue}")
+    private String[] listOfValue;
+
+    @Value("${config.code}")
+    private Integer code;
+
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
     }
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("Producto", product);
+        json.put("Id", id);
+        return json;
+    }
+    @PostMapping("/create")
+    public user  create(@RequestBody user user) {
+       
+        return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values() {
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("message", message);
+        json.put("code", code);
+        json.put("listOfValues", listOfValue);
+        return json;
+    }
+    
     
 }
